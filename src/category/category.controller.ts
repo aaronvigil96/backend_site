d8bb502ddccd8e77fe3472f9213616f87e7a237a
aaronvigil96 <aaronvigil96@gmail.com>
2025-01-31 22:22:52 -0300

    fixed response with throttle

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -2,17 +2,20 @@ import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGua
 import { CategoryService } from "./category.service";
 import { CreateCategoryDto } from "./dto/create-category.dto";
 import { AuthGuard } from "@nestjs/passport";
+import { SkipThrottle } from "@nestjs/throttler";
 
 @Controller('categories')
 export class CategoryController {
 
     constructor(private readonly categoryService:CategoryService){}
 
+    @SkipThrottle()
     @Get()
     getAll(){
         return this.categoryService.getAll();
     }
 
+    @SkipThrottle()
     @Get(':id')
     getById(@Param('id', ParseIntPipe )id:number){
         return this.categoryService.getById(id);
